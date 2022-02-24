cc5631cabf96855d7bd5b8b6dc437d997ce1b022
chineduCoded <chinedujohn17@gmail.com>
2022-02-24 21:31:47 +0100

    page  not found

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,6 +1,51 @@
 import React from 'react'
+import styled from 'styled-components'
+import img from '../assets/404.png'
+import PowerButton from '../subComponents/PowerButton'
+
+const Container = styled.div`
+  width: 100%;
+  padding: 2rem;
+  background: #000;
+  height: 100vh;
+`
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 150px;
+`
+const ImageHolder = styled.div`
+  width: min(400px, 100%);
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+
+  div {
+    text-align: center;
+    color: #fff;
+    font-weight: 600;
+    font-size: 18px;
+  }
+`
+
+const Image = styled.img`
+  object-fit: cover;
+`
 
 const PageNotFound = () => {
-  return <div>Page not found</div>
+  return (
+    <Container>
+      <PowerButton />
+      <Wrapper>
+        <ImageHolder>
+          <Image src={img} alt="page not found" />
+          <div>Page Not Found</div>
+        </ImageHolder>
+      </Wrapper>
+    </Container>
+  )
 }
 export default PageNotFound
